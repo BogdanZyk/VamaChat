c846420e090270d8173efe7a8de28a324241bc02
Bogdan <zykovbogdan07@gmail.com>
2023-07-31 16:22:26 +0300

    Initial Commit

---
+++ Vama/VamaApp.swift
@@ -0,0 +1,17 @@
+//
+//  VamaApp.swift
+//  Vama
+//
+//  Created by Bogdan Zykov on 31.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct VamaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
